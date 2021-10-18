3e79919f9b4ab5621daf8c1994747049940f12ee
samuelguzmanajala <samuelguzmanajala@gmail.com>
2021-10-18 23:32:23 +0200

    CL

---
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -0,0 +1,11 @@
+package edu.ncsu.monopoly;
+//updated 2021
+public abstract class Card {
+
+    public static final int TYPE_CHANCE = 1;
+    public static final int TYPE_CC = 2;
+
+    public abstract String getLabel();
+    public abstract void applyAction();
+    public abstract int getCardType();
+}
